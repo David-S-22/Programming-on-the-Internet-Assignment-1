5c078e0b416fa3308ecc1c0c3608c629afb9c39d
David Sorrell <david2017au@gmail.com>
2026-03-22 00:25:33 +1100

    improving table styling

--- frontend/script.js
+++ frontend/script.js
@@ -1,48 +1,78 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function populateExpenseTable() {
-  fetch("http://localhost:3000/expenses")
-    .then((data) => {
-      data.json()
-      .then((expenses) => {
-        for (var expense of expenses) {
-          const tableBody = document.getElementById("expense-table-body");
-          tableBody.appendChild(createExpenseRow(expense))
-        }
-      })
-      .catch((e) => console.log(e))
+    fetch("http://localhost:3000/expenses")
+        .then(function (data) {
+        data.json()
+            .then(function (expenses) {
+            for (var _i = 0, expenses_1 = expenses; _i < expenses_1.length; _i++) {
+                var expense = expenses_1[_i];
+                var tableBody = document.getElementById("expense-table-body"); //Getting the table body so we can dynamically add table rows based on the data fetched from the db into it
+                tableBody.appendChild(createExpenseRow(expense));
+            }
+        })
+            .catch(function (e) { return console.log(e); });
     })
-    .catch((e) => console.log(e))
+        .catch(function (e) { return console.log(e); });
 }
-
 function createExpenseRow(expense) {
-  const expenseTitleElement = document.createElement("th");
-  expenseTitleElement.innerText = expense.title;
-  const expenseCategoryElement = document.createElement("td");
-  expenseCategoryElement.innerText = expense.category;
-  const expenseAmountElement = document.createElement("td");
-  expenseAmountElement.innerText = expense.amount;
-  const expenseCostElement = document.createElement("td");
-  expenseCostElement.innerText = expense.cost;
-  const expenseDateElement = document.createElement("td");
-  expenseDateElement.innerText = expense.date.toString();
-  const expenseDescriptionElement = document.createElement("td");
-  expenseDescriptionElement.innerText = expense.description;
-
-
-  const editActionButton = document.createElement("span");
-  editActionButton.innerText = "✎"
-  const deleteActionButton = document.createElement("span");
-  deleteActionButton.innerText = "🗑"
-  const expenseActions = document.createElement("td");
-  expenseActions.append(editActionButton);
-  expenseActions.append(deleteActionButton);
-
-  const expenseTableRow = document.createElement("tr");
-  expenseTableRow.appendChild(expenseTitleElement);
-  expenseTableRow.appendChild(expenseCategoryElement)
-  expenseTableRow.appendChild(expenseAmountElement)
-  expenseTableRow.appendChild(expenseCostElement)
-  expenseTableRow.appendChild(expenseDateElement)
-  expenseTableRow.appendChild(expenseDescriptionElement);
-  expenseTableRow.appendChild(expenseActions);
-  return expenseTableRow;
+    var expenseTitleTableData = document.createElement("td");
+    var expenseTitleSpan = document.createElement("span");
+    expenseTitleSpan.className = "expense-table-data";
+    expenseTitleSpan.innerText = expense.title;
+    expenseTitleTableData.appendChild(expenseTitleSpan);
+    var expenseCategoryTableData = document.createElement("td");
+    var expenseCategorySpan = document.createElement("span");
+    expenseCategorySpan.className = "expense-table-data";
+    expenseCategorySpan.innerText = expense.category;
+    expenseCategoryTableData.appendChild(expenseCategorySpan);
+    var expenseAmountTableData = document.createElement("td");
+    var expenseAmountSpan = document.createElement("span");
+    expenseAmountSpan.className = "expense-table-data";
+    expenseAmountSpan.innerText = expense.amount.toString();
+    expenseAmountTableData.appendChild(expenseAmountSpan);
+    var expenseCostTableData = document.createElement("td");
+    var expenseCostSpan = document.createElement("span");
+    expenseCostSpan.className = "expense-table-data";
+    expenseCostSpan.innerText = expense.cost.toString();
+    expenseCostTableData.appendChild(expenseCostSpan);
+    var expenseDateTableData = document.createElement("td");
+    var expenseDateSpan = document.createElement("span");
+    expenseDateSpan.className = "expense-table-data";
+    expenseDateSpan.innerText = expense.date.toString();
+    expenseDateTableData.appendChild(expenseDateSpan);
+    var expenseDescriptionTableData = document.createElement("td");
+    var expenseDescriptionSpan = document.createElement("span");
+    expenseDescriptionSpan.className = "expense-table-data";
+    expenseDescriptionSpan.innerText = expense.description;
+    expenseDescriptionTableData.appendChild(expenseDescriptionSpan);
+    var editActionButton = document.createElement("button");
+    editActionButton.innerText = "✎";
+    editActionButton.className = "expense-table-action-button";
+    editActionButton.onclick = function () { return updateExpense(expense.id); };
+    var deleteActionButton = document.createElement("button");
+    deleteActionButton.innerText = "🗑";
+    deleteActionButton.className = "expense-table-action-button";
+    deleteActionButton.onclick = function () { return deleteExpense(expense.id); };
+    var expenseActionsSpan = document.createElement("span");
+    expenseActionsSpan.className = "expense-table-data";
+    expenseActionsSpan.append(editActionButton);
+    expenseActionsSpan.append(deleteActionButton);
+    var expenseActionsTableData = document.createElement("td");
+    expenseActionsTableData.appendChild(expenseActionsSpan);
+    var expenseTableRow = document.createElement("tr");
+    expenseTableRow.id = expense.id.toString();
+    expenseTableRow.appendChild(expenseTitleTableData);
+    expenseTableRow.appendChild(expenseCategoryTableData);
+    expenseTableRow.appendChild(expenseAmountTableData);
+    expenseTableRow.appendChild(expenseCostTableData);
+    expenseTableRow.appendChild(expenseDateTableData);
+    expenseTableRow.appendChild(expenseDescriptionTableData);
+    expenseTableRow.appendChild(expenseActionsTableData);
+    return expenseTableRow;
+}
+function updateExpense(expenseToUpdateId) {
+    console.log(expenseToUpdateId);
+}
+function deleteExpense(expenseToRemoveId) {
 }
