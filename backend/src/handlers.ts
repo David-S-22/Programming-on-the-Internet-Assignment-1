f792e5db924a79faaf57c97da174d2fa5cf4a1d2
David Sorrell <david2017au@gmail.com>
2026-04-06 11:41:31 +1000

    Beginning to refactor routes into their own context

--- backend/src/handlers.ts
+++
@@ -1,1 +0,0 @@
-import type { FastifyRequest } from "fastify";
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import Fastify from 'fastify'
 import fastifypg from '@fastify/postgres'
 import dotenv from "dotenv";
-import type { ExpenseCreationRequest, ExpenseIdRequest, ExpenseUpdateRequest, ExpenseQueryRequest } from './types.js';
-import type { expense } from '../../common/types.js';
-import { format, subMonths } from 'date-fns';
+import type { ExpenseCreationRequest, ExpenseUpdateRequest } from './types.js';
+import type { expense } from '../../common/types.d.ts';
+import requestHandlerContext from './requestHandlers.ts';
 
 dotenv.config({ path: "./.env", override: true });
 
@@ -19,35 +19,7 @@ fastify.register(fastifypg, {
   database: process.env.PGDATABASE
 });
 
-fastify.get<ExpenseQueryRequest>("/expenses", async (req, res) => {
-  let { category, period } = req.query;
-  category = category ?? "";
-
-  //If a value is provided we use that to determine the earliest expense time to get. 
-  //Otherwise we get all of the expenses by using `-infinity` as it's a special value that is below all other values in postgres
-  const earliestExpenseDate = period != null ? format(subMonths(new Date(), period), "yyyy-MM-dd") : '-infinity';
-
-  const foundExpenses = (await fastify.pg.query<expense>(`SELECT * FROM expenses.expense 
-                                                         WHERE (category = $1 OR '' = $1) AND (date >= $2)
-                                                         ORDER BY id`, [category, earliestExpenseDate])).rows;
-
-  res.header("Access-Control-Allow-Origin", "*");
-  res.status(200);
-  res.send(JSON.stringify(foundExpenses));
-});
-
-fastify.delete<ExpenseIdRequest>("/expenses/:id", async (req, res) => {
-  const { id } = req.params;
-  const result = (await fastify.pg.query("DELETE from expenses.expense WHERE id = $1", [id])).rowCount;
-
-  if (result === 0) {
-    res.status(404);
-    res.send("Failed to delete expense as it does not exist.");
-  }
-
-  res.header("Access-Control-Allow-Origin", "*");
-  res.status(200);
-});
+fastify.register(requestHandlerContext);
 
 fastify.patch<ExpenseUpdateRequest>("/expenses/:id", async (req, res) => {
   const { id } = req.params;
