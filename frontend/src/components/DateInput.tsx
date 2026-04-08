b54ea2cb377f6bda496856186fb723b3ec746168
David Sorrell <david2017au@gmail.com>
2026-04-08 16:56:24 +1000

    Fixing up prop typing for date input

--- frontend/src/components/DateInput.tsx
+++ frontend/src/components/DateInput.tsx
@@ -1,7 +1,14 @@
 import { format, isValid } from "date-fns";
 
+type DateInputProps = {
+  value : Date
+  onChange : (date : Date) => void
+  ariaLabel? : string
+  title? : string
+}
+
 //The main purpose of this is to allow me to properly format and get the value from the date input. This is because we need to format the value so that we can display it as the value, and also it needs to be able to handle nulls gracefully incase the user does not input a date.
-function DateInput(props : { value: Date, onChange: (date: Date) => void, ariaLabel?: string, title?: string }) {
+function DateInput(props : DateInputProps) {
   return (
     <input
       aria-label={props.ariaLabel}
